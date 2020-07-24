ab4ba6b13276b471c968ed0047ce1023fe8a0906
Dries Cruyskens <dries.cruyskens@gmail.com>
2020-07-24 14:26:07 +0100

    Used the right chip-8 specs this time hopefully.

--- src/chip8.rs
+++ src/chip8.rs
@@ -136,7 +136,7 @@ impl Chip8 {
                         }
                     }
                     /* Set Vx = Vx - Vy, set VF = NOT borrow.
-                    If Vx > Vy, then VF is set to 1, otherwise 0. 
+                    If Vx > Vy, then VF is set to 1, otherwise 0.
                     Then Vy is subtracted from Vx, and the results stored in Vx. */
                     0x5 => {
                         let (subtraction, borrow) = self.v[x].overflowing_sub(self.v[y]);
@@ -147,14 +147,14 @@ impl Chip8 {
                             self.v[0xF] = 1;
                         }
                     }
-                    /* Set Vx = Vx SHR 1.
-                    If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. 
-                    Then Vx is divided by 2. */
+                    /* Store the value of register VY shifted right one bit in register VX¹
+                    Set register VF to the least significant bit prior to the shift
+                    VY is unchanged. */
                     0x6 => {
-                        self.v[0xF] = self.v[x] & 0x1;
-                        self.v[x] = self.v[x] >> 1;
+                        self.v[0xF] = self.v[y] & 0x1;
+                        self.v[x] = self.v[y] >> 1;
                     }
-                    /* Set Vx = Vy - Vx, set VF = NOT borrow. 
+                    /* Set Vx = Vy - Vx, set VF = NOT borrow.
                     If Vy > Vx, then VF is set to 1, otherwise 0.
                     Then Vx is subtracted from Vy, and the results stored in Vx. */
                     0x7 => {
@@ -166,12 +166,12 @@ impl Chip8 {
                             self.v[0xF] = 1;
                         }
                     }
-                    /* Set Vx = Vx SHL 1.
-                    If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. 
-                    Then Vx is multiplied by 2. */
+                    /* Store the value of register VY shifted left one bit in register VX
+                    Set register VF to the most significant bit prior to the shift
+                    VY is unchanged. */
                     0xE => {
-                        self.v[0xF] = (self.v[x] & 0x80) >> 7;
-                        self.v[x] = self.v[x] << 1;
+                        self.v[0xF] = (self.v[y] & 0x80) >> 7;
+                        self.v[x] = self.v[y] << 1;
                     }
                     _ => unreachable!(),
                 }
@@ -201,9 +201,9 @@ impl Chip8 {
             }
             /* Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
             The interpreter reads n bytes from memory, starting at the address stored in I.
-            These bytes are then displayed as sprites on screen at coordinates (Vx, Vy). 
-            Sprites are XORed onto the existing screen. If this causes any pixels to be erased, 
-            VF is set to 1, otherwise it is set to 0. 
+            These bytes are then displayed as sprites on screen at coordinates (Vx, Vy).
+            Sprites are XORed onto the existing screen. If this causes any pixels to be erased,
+            VF is set to 1, otherwise it is set to 0.
             If the sprite is positioned so part of it is outside the coordinates of the display,
             it wraps around to the opposite side of the screen. */
             0xD => {
@@ -306,19 +306,23 @@ impl Chip8 {
                         self.pc += 2;
                     }
                     /* Store registers V0 through Vx in memory starting at location I.
-                    The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I. */
+                    The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I.
+                    I is set to I + X + 1 after operation. */
                     0x55 => {
                         for i in 0..=x {
                             self.mem[self.i + i] = self.v[i];
                         }
+                        self.i = self.i + x + 1;
                         self.pc += 2;
                     }
                     /* Read registers V0 through Vx from memory starting at location I.
-                    The interpreter reads values from memory starting at location I into registers V0 through Vx. */
+                    The interpreter reads values from memory starting at location I into registers V0 through Vx.
+                    I is set to I + X + 1 after operation. */
                     0x65 => {
                         for i in 0..=x {
                             self.v[i] = self.mem[self.i + i];
                         }
+                        self.i = self.i + x + 1;
                         self.pc += 2;
                     }
                     _ => unreachable!(),
